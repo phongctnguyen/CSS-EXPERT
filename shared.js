6c21ed8e334e95ee1678513c38fee47f84099432
phongctnguyen <phongctnguyen@gmail.com>
2020-08-21 23:10:12 +0700

    finish css course

--- shared.js
+++ shared.js
@@ -4,8 +4,9 @@ const modalNoButton = document.querySelector('.modal__action--negative');
 const selectPlanButtons = document.querySelectorAll('.plan button');
 const toggleButton = document.querySelector('.toggle-button');
 const mobileNav = document.querySelector('.mobile-nav');
+const ctaButton = document.querySelector(".main-nav__item--cta");
 
-console.dir(backdrop)
+// console.dir(backdrop)
 
 for (let button of selectPlanButtons) {
   button.addEventListener('click', () => {
@@ -13,6 +14,7 @@ for (let button of selectPlanButtons) {
     // modal.style.display = 'block';
     modal.classList.add('open');
     backdrop.classList.add('open');
+    backdrop.style.display = 'block';
   });
 }
 
@@ -22,7 +24,10 @@ const closeModal = () => {
   if (modal) {
     modal.classList.remove("open");
   }
-  backdrop.classList.remove("open");
+  backdrop.classList.remove('open');
+  setTimeout(() => {
+    backdrop.style.display = 'none';
+  }, 10)
 };
 
 backdrop.addEventListener('click', () => {
@@ -39,5 +44,20 @@ toggleButton.addEventListener('click', () => {
   // mobileNav.style.display = 'block';
   // backdrop.style.display = 'block';
   mobileNav.classList.add('open');
-  backdrop.classList.add('open');
+  backdrop.style.display = 'block';
+  setTimeout(() => {
+    backdrop.classList.add('open');
+  }, 10)
 });
+
+ctaButton.addEventListener('animationstart', (e) => {
+  console.log(e);
+})
+
+ctaButton.addEventListener('animationend', (e) => {
+  console.log(e);
+})
+
+ctaButton.addEventListener('animationiteration', (e) => {
+  console.log(e);
+})
